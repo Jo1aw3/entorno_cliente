46b536599eba14f98cb0ebc2a1e28ba643d3592a
Jo1aw3 <joshuabotello2003@gmail.com>
2024-01-15 09:54:12 +0100

    mega up

--- entorno_cliente01/menus_jscript/JS/modal.js
+++ entorno_cliente01/menus_jscript/JS/modal.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-var elemVolver = document.getElementById("elemVolver");
-elemVolver.addEventListener("click", function() {
-    window.location.href = "INDEX.html";
-});
+    var elemVolver = document.getElementById("elemVolver");
+    elemVolver.addEventListener("click", function() {
+        window.location.href = "INDEX.html";
+    });
 
-var url = window.location.href;
-var recorte = url.split("/");
+    var url = window.location.href;
+    var recorte = url.split("/");
 
-var platosSeleccionados = [];
+    var platosSeleccionados = [];
 
-if (recorte[recorte.length - 1] == "ELEGIR_PRODUCTO.html?view=platos" || recorte[recorte.length - 1] == "ELEGIR_PRODUCTO.html?view=platos#") {
-    $('.navMenu').hide();
-} else {
-    $('.navMenu').show();
-}
+    if (recorte[recorte.length - 1] == "ELEGIR_PRODUCTO.html?view=platos" || recorte[recorte.length - 1] == "ELEGIR_PRODUCTO.html?view=platos#") {
+        $('#comboMenus').hide();
+    }
 
+
+    
 });
