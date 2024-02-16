b79c5f5ab0729d9693d69b91cf2b5122c63a3942
Jo1aw3 <joshuabotello2003@gmail.com>
2024-02-16 14:54:47 +0100

    up

---
+++ entorno_cliente02/formulario_jscript/bigarrenLehioa.js
@@ -0,0 +1,11 @@
+// const colEmail = document.getElementById("emailTable");
+// const colPass = document.getElementById("firstnameTable");
+// const colLast = document.getElementById("lastnameTable");
+// const colDate = document.getElementById("");
+// const colSubj = document.getElementById("subjectTable");
+
+
+
+document.getElementById("firstnameTable").innerHTML = document.getElementById("correo").value;
+
+console.log(colEmail);
