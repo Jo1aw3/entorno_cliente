d5d4bd239075824b5434aad4330104ea64843fd2
Joshua <123463629+Jo1aw3@users.noreply.github.com>
2024-02-18 17:02:12 +0100

    fixing

--- entorno_cliente00/readme.txt
+++
@@ -1,2 +0,0 @@
-Entorno Cliente 00 contiene contenido adicional dedicada al autoaprendizaje: 
-para comprender y entender JavaScrip y respectivos frameworks
--- entorno_cliente02/camiones_angular/js/gestion_camiones.js
+++ entorno_cliente02/camiones_angular/js/gestion_camiones.js
@@ -1,41 +1,41 @@
-angular.module("app", []).controller("TruckController", function ($scope, $http) {
-  $scope.trucks = [];
-//   {
-//     id: 1,
-//     modelo: "Camión A",
-//     marca: "Marca X",
-//     ano: 2020,
-//     imagen: "img/camion1.jpeg",
-//   },
-//   {
-//     id: 2,
-//     modelo: "Camión B",
-//     marca: "Marca Y",
-//     ano: 2022,
-//     imagen: "img/camion2.jpeg",
-//   },
-//   {
-//     id: 3,
-//     modelo: "Camión D",
-//     marca: "Marca Z",
-//     ano: 2019,
-//     imagen: "img/camion3.jpeg",
-//   },
+angular.module("app", []).controller("controller", function ($scope, $http) {
+
+  $scope.trucks = [
+    /*
+      {
+        id: 1,
+        modelo: "Camión A",
+        marca: "Marca X",
+        ano: 2020,
+        imagen: "img/camion1.jpeg"
+      },
+      {
+        id: 2,
+        modelo: "Camión B",
+        marca: "Marca Y",
+        ano: 2022,
+        imagen: "img/camion2.jpeg"
+      },
+      {
+        id: 3,
+        modelo: "Camión D",
+        marca: "Marca Z",
+        ano: 2019,
+        imagen: "img/camion3.jpeg"
+      }
+    */
+  ];
 
   $scope.newTruck = {};
 
-  $http.get("json/optimus.json")
+  $http.get("json/camiones.json")
     .then(function (response) {
       $scope.trucks = response.data;
-      // console.log($scope.trucks);
+      console.log($scope.trucks);
     })
     .catch(function (error) {
       console.error("Error al cargar el archivo JSON:", error);
     });
-
-  $scope.displayTrucks = function (truckArray) {
-    $scope.trucks = truckArray;
-  };
 
   $scope.showTruckDetails = function (truck) {
     alert(
@@ -52,12 +52,7 @@ angular.module("app", []).controller("TruckController", function ($scope, $http)
     $scope.newTruck.id = $scope.trucks.length + 1;
     $scope.trucks.push($scope.newTruck);
     $scope.newTruck = {};
-    // console.log($scope.trucks);
+    console.log($scope.trucks);
   };
 
-  $scope.toggleFormMargin = function (isHover) {
-    // Función para animación de formulario
-    var form = document.getElementById("add-truck-form");
-    form.style.marginTop = isHover ? "0" : "50px";
-  };
 });
