85e138fd5b664d19bb4583c25bc35e15e505e471
Jo1aw3 <joshuabotello2003@gmail.com>
2024-03-13 12:56:45 +0100

    cards

--- examen_final/aztEasyLearning_Angular_ikaslea/public_html/js/jslogin.js
+++ examen_final/aztEasyLearning_Angular_ikaslea/public_html/js/jslogin.js
@@ -11,6 +11,7 @@ angular
     $scope.submitForm = function () {
       $scope.logMal = true;
       var esAdmin = false;
+      var idUsuario;
 
       // Verificar correo y contraseña
       angular.forEach($scope.usuarios, function (usuario) {
@@ -19,18 +20,27 @@ angular
           usuario.password == $scope.usuario.contra
         ) {
           $scope.logMal = false;
+          // como despues del bucle el "usuario" deja de existir, 
+          // en una variable anteriormente creada guardo el valor del id del usuario
+          idUsuario = usuario.id;
           if (usuario.userType == "admin") {
             esAdmin = true;
-          }
+          } 
         }
       });
 
       if (!$scope.logMal) {
-        alert("ta bien todo, login correcto");
+        alert("login correcto");
+        console.log(idUsuario);
         if (esAdmin) {
           $window.location.href = "administration.html";
         } else {
+          localStorage.setItem('id', idUsuario); 
           $window.location.href = "GetStarted.html";
+
+          // como se cambia de pagina, las siguientes lineas comentadas no funcionan
+          // var data = JSON.stringify(usuario);
+          // localStorage.setItem('id', usuario.id);
         }
       } else {
         alert("ta to mal, login incorrecto");
