1a91b1e3b2ede502c2d43ca87bf6704485d78742
Jo1aw3 <joshuabotello2003@gmail.com>
2024-04-10 09:50:57 +0200

    eventos

--- entorno_cliente_exm/eventos_js/1_EVENTOSYFORMATOS.js
+++ entorno_cliente_exm/eventos_js/1_EVENTOSYFORMATOS.js
@@ -2,6 +2,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Selecciono las imagenes del contenedor por medio de su id
     var imgTop = document.querySelectorAll("#topDiv>img");
+    // Se pueden seleccionar los elementos por separado de esta manera
+    // var imgTopL = document.querySelector("#topDiv>img:first-child");
+    // var imgTopR = document.querySelector("#topDiv>img:last-child");
     var imgMiddle = document.querySelectorAll("#middleDiv>img");
 
     // No es necesario recorrer los "querySelector", nomás para saber si se han seleccionado correctante (en este caso las imagenes)
@@ -33,4 +36,75 @@ document.addEventListener("DOMContentLoaded", function() {
         
     });
 
-});
+    // Se puede crear una variable para obtener el elemento del body, y usarlo en las siguientes funciones
+    cuerpo = document.querySelector("body");
+
+    imgMiddle.forEach((imagen, index)=> {
+        imagen.addEventListener("mouseover",fondoBlanco);
+        imagen.addEventListener("mouseout",fondoNegro);
+        imagen.addEventListener("click", function() {
+            selectImg(index);
+        });
+    });
+
+    function fondoBlanco() {
+        // document.querySelector("body").style.backgroundColor = 'white';
+        // document.querySelector("body").style.color = 'black';
+        cuerpo.style.backgroundColor = 'white';
+        cuerpo.style.color = 'black';
+    }
+    function fondoNegro() {
+        cuerpo.style.backgroundColor = 'black';
+        cuerpo.style.color = 'white';
+    }
+
+    // Creo Arrays para guardar las URL de la imagenes
+    var animales = ["images/animal1.jpg","images/animal2.jpg","images/animal3.jpg"]
+    var paisajes = ["images/paisaje1.jpg","images/paisaje2.jpg","images/paisaje3.jpg"]
+    var personas = ["images/persona1.jpg","images/persona2.jpg","images/persona3.jpg"]
+    
+    contenedor = document.getElementById("bottomDiv");
+    
+    function selectImg(numIndex) {
+        console.log(numIndex);
+        switch (numIndex) {
+            case 0:
+                mostrarImagenes(animales);
+                break;
+                case 1:
+                    mostrarImagenes(paisajes);
+                    break;
+                    case 2:
+                        mostrarImagenes(personas);
+                        break;
+                    }
+                }
+                
+                function mostrarImagenes(listaImg) {
+                    contenedor.innerHTML = "";
+                    listaImg.forEach((imagen, index)=> {
+                        contenedor.innerHTML += `<img src="${imagen}">`;
+                    });
+                }
+                
+                var contBottom = document.querySelector("#bottomDiv");
+                contBottom.addEventListener("click", (e) => {
+                    console.log(e);
+                    console.log(e.target);
+
+                    if (e.target.nodeName == "IMG") {
+                        let newWin = window.open("about:blank", "Imagen Seleccionada", "width=720,height=720");
+                        let texto = `<h1>Has seleccionado esta imagen:</h1>
+                                    <img src="${e.target.getAttribute("src")}"></img>`;
+                        newWin.document.write(texto);
+                    }
+                });
+
+                // imgBottom.forEach((imagen, index)=> {
+                //     imagen.addEventListener("click", function() {
+                //         window.open("Popup", "width=900, height=700")
+                //         window.innerHTML = `<p>Hemos echo click en la imagen</p>`;
+                //     });
+                // });
+                
+            });
