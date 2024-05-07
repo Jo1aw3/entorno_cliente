3a107024b496807d4b0024583cf559581a93ef64
Joshua <123463629+Jo1aw3@users.noreply.github.com>
2024-05-07 14:10:53 +0200

    puzzle

---
+++ entorno_cliente_exm/adivina_js/js/azterketa.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener("DOMContentLoaded", load, false);
+var listaPalabras=['AZTERKETA','ORNITORRINCO','CEFALORAQUIDEO','MENDIKATEA'];
+var user="user"
+var passwd= "user"
+var palabra ="ELEFANTE";
+
+
+function load() {   
+
+
+};    
+ 
+
+
+
