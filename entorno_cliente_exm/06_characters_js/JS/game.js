c52f195bb5b08dcaa26b5cf74905081379c844f0
Jo1aw3 <joshuabotello2003@gmail.com>
2024-05-08 09:29:01 +0200

    files changes

---
+++ entorno_cliente_exm/06_characters_js/JS/game.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", load, false);
+
+function load() {
+  
+   imgAll=["IMAGES/allCharactersOpac.png","IMAGES/allPlayersOpac.png","IMAGES/allGamesOpac.png"]
+   textoAll=["CHARACTERS","PLAYERS","GAMES"]
+
+}
+ 
