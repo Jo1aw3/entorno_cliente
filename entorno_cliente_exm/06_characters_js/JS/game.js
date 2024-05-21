9b52a64851ae0c85c0c5eaf86ff16ba8ff575ae7
Joshua <123463629+Jo1aw3@users.noreply.github.com>
2024-05-21 19:45:18 +0200

    characters

--- entorno_cliente_exm/06_characters_js/JS/game.js
+++ entorno_cliente_exm/06_characters_js/JS/game.js
@@ -37,7 +37,7 @@ function load() {
    const btnLogin = document.getElementById("btnLogin");
    const idPlayer = document.getElementById("idPlayer");
    const playerData = document.getElementById("playerData");
-   // const imgPlayer = document.querySelector("#idPlayer>img");
+   // const imgPlayer = document.querySelector("#playerData>img");
    const btnDelete = document.getElementById("btnDelete");
    const btnSave = document.getElementById("btnSave");
    const btnCancel = document.getElementById("btnCancel");
@@ -100,6 +100,21 @@ function load() {
       });
       imgAbajo.innerHTML = imgFree;
    });
+   // btnFree.addEventListener("click", personajes_libres);
+   // function personajes_libres() {
+   //    console.log("mostrando los personajes libres");
+   //    var imgFree = "";
+   //    personajes.forEach(dato => {
+   //       if (dato.libre == "BAI") {
+   //          imgFree += `
+   //          <div class='caja'>
+   //             <img class='abajo' id="${dato.id}" src="${dato.imgCharac}"/>
+   //          </div>
+   //          `;
+   //       }
+   //    });
+   //    imgAbajo.innerHTML = imgFree;
+   // }
 
    // para mostrar todos los personajes
    btnAll.addEventListener("click", function() {
@@ -123,24 +138,37 @@ function load() {
       divForms.style.display = "block";
    });
    
+   // algunas variables para la informacion del jugador
+   var chosen = "";
+   var imgChosen = "";
+   var idJugador = "";
+   var idCharacter = "";
+
    // para validar el formulario
    btnLogin.addEventListener("click", function() {
       console.log("validando el formulario");
       
-      validar = false;
-      chosen = "";
-      imgChosen = "";
+      var validar = false;
+      var validarPersonaje = false;
 
       jugadores.forEach(dato => {
          if (user.value == dato.nick && pass.value == dato.password) {
             validar = true;
+            idJugador = dato.id;
 
             personajes.forEach(datoC => {
                if (dato.character == datoC.id) {
+                  idCharacter = datoC.id;
                   chosen = datoC.name;
                   imgChosen = datoC.imgCharac;
+                  validarPersonaje = true;
                }
             });
+
+            if (!validarPersonaje) {
+               chosen = "";
+               imgChosen = "IMAGES/noImage.png";
+            }
 
             playerData.innerHTML = `
                <h2>PLAYER :  ${dato.nick}</h2>
@@ -160,19 +188,63 @@ function load() {
 
    // para soltar el personaje del jugador
    btnDelete.addEventListener("click", function() {
-
+      
+      let personajeLibre = false;
+      
+      jugadores.forEach(dato => {
+         if (dato.id === idJugador) {
+            // console.log("quitando personaje " + dato.character)
+            dato.character = -1;
+            personajeLibre = true;
+            console.log(jugadores);
+         }
+      });
+      
+      if (personajeLibre) {
+         personajes.forEach(dato => {
+            if (dato.id === idCharacter) {
+               dato.libre = "BAI";
+               console.log("personaje libre: " + dato.name)
+            }
+         });
+      }
+      alert("soltando tu personaje, espere a que se guarden los cambios...");
+      setTimeout(function() {
+         alert("se han guardado correctamente los cambios");
+         ocultar_contenido();
+         divAbajo.style.display = "block";
+      }, 1000);
    });
 
    // para elegir un personaje al jugador
    btnSave.addEventListener("click", function() {
+      // console.log("eligiendo personaje");
 
+      do {
+         var max = personajes.length -1;
+         var r = Math.round(Math.random() * (max - 0) + 0);
+         console.log(personajes[r].libre);
+      } while (personajes[r].libre == "EZ");
+      
+      jugadores.forEach(dato => {
+         if (dato.id === idJugador) {
+            dato.character = personajes[r].id;
+            personajes[r].libre = "EZ";
+            console.log(jugadores[idJugador]);
+            alert("Mira!, " + personajes[r].name + " es tu nuevo personaje. Esperando a que se guarden los datos");
+            setTimeout(function() {
+               alert("se han guardado correctamente los cambios");
+               ocultar_contenido();
+               divAbajo.style.display = "block";
+            }, 1000);
+         }
+      });
    });
 
    // para salir de la información del jugador
    btnCancel.addEventListener("click", function() {
       ocultar_contenido();
-      divAbajo.style.display="block";
-
+      divAbajo.style.display = "block";
    });
 }
 
