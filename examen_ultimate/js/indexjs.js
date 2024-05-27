ad55ff2d8987bb46a692aa747e6896790db9110e
Jo1aw3 <joshuabotello2003@gmail.com>
2024-05-27 09:17:14 +0200

    hola

---
+++ examen_ultimate/js/indexjs.js
@@ -0,0 +1,10 @@
+var listaHitzak =["oca","usoa","perro","hartza","ardilla","basurdea","cocodrilo","barraskiloa","rinoceronte","marigorringo","mantis religiosa","ornitorrinko"];
+
+document.addEventListener("DOMContentLoaded", load, false);
+
+
+function load() {
+   
+      }
+          
+ 
